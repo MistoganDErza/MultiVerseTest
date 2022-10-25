3e683ac74f0d0bce71136a8ffb5b3a61c47e5996
MistoganErza <phungwingkit@gmail.com>
2022-10-25 10:29:31 +0200

    First Commit

---
+++ MultiVerseTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiVerseTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World Master!");
+        }
+    }
+}
